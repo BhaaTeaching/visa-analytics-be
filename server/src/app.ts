2fce93a29806061ff456f7ae432d413b67e1b777
BhaaTeaching <bhaa.teaching@gmail.com>
2023-10-10 23:42:13 +0300

    add greeting

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from "express";
 import { add, readeExcelFile } from "./services/files/FileService";
 
 const app = express();
-
+app.get("/greeting", (req: Request, res: Response) => {
+  res.send("hello world");
+});
 app.get("/", (req: Request, res: Response) => {
   const fileContent = readeExcelFile();
   console.log("request reached ...");
